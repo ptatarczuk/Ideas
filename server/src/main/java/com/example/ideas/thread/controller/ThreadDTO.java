92e377af85dc8a9ca6bb47bdb912ccd307f22223
X250 <aleksander.synoradzki@gmail.com>
2023-09-04 13:57:37 +0200

    ThreadService modified - only the author of the Thread can modify it with PATCH method

--- server/src/main/java/com/example/ideas/thread/controller/ThreadDTO.java
+++ server/src/main/java/com/example/ideas/thread/controller/ThreadDTO.java
@@ -1,25 +1,17 @@
 package com.example.ideas.thread.controller;
 
 import com.example.ideas.admission.model.Admission;
-import com.example.ideas.comment.model.Comment;
 import com.example.ideas.conclusion.model.Conclusion;
-import com.example.ideas.user.model.User;
 import com.example.ideas.util_Entities.category.model.Category;
 import com.example.ideas.util_Entities.stage.model.Stage;
 import com.example.ideas.util_Entities.status.model.Status;
-import com.example.ideas.util_Entities.vote.model.Vote;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
-
-
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
 
 @AllArgsConstructor
 @Getter
 public class ThreadDTO {
+    private String email;
     private String title;
     private String description;
     private String justification;
