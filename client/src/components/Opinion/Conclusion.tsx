bc2380cdfb2568aac7d5f4aa5d9af978d0fe133f
JowitaDruciak <jowita.druciak@gmail.com>
2023-09-14 12:08:25 +0200

    add conclusion

--- client/src/components/Opinion/Conclusion.tsx
+++ client/src/components/Opinion/Conclusion.tsx
@@ -1,32 +1,96 @@
+import React, { useState } from 'react';
+import { Avatar, Grid, Divider, Paper, TextField, Button } from '@mui/material';
 import { Thread } from '../../models/Thread';
-
 
 interface ConclusionProps {
     thread: Thread;
     decodedToken: any;
 }
 
-
 export const Conclusion: React.FC<ConclusionProps> = ({ thread, decodedToken }) => {
-    const isUser = decodedToken.role === "User";
-    const isAdmin = decodedToken.role === "Admin";
+    const isAdmin = decodedToken.role === 'Admin';
+    const [newConclusion, setNewConclusion] = useState<string>('');
 
-    
+//ściezka do colclusion + zastanowić sie co ze stagem
+
+    const handleAddConclusion = async (stage: string) => {
+        try {
+            const dataToSend = {
+                stage: stage,
+                conclusion:
+                {
+                    content: newConclusion
+                }
+            };
+
+            console.log(dataToSend);
+            const response = await fetch(`http://localhost:8080/threads/id999/${thread.threadId}`, {
+                method: 'PATCH',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(dataToSend),
+            });
+
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+
+        } catch (error) {
+            console.error('An error occurred while saving data', error);
+        }
+
+        setNewConclusion('');
+    };
+
+
+
+
     return (
         <div>
-          {thread.conclusion ? (
-            // Wyświetl istniejące konkluzje
-            <div>
-              <h2>Conclusion:</h2>
-              <ul>
-                <h1>{thread.conclusion.content}</h1>
-              </ul>
-            </div>
-          ) : null}
-          {isAdmin ? (
-            <div>
-              <button>Add Conclusion:</button>
-        </div>): null}
+            {thread.conclusion ? (
+                <Paper style={{ padding: '40px 20px' }}>
+                    <h2>Conclusion:</h2>
+                    <Grid container wrap="nowrap" spacing={2}>
+                        <Grid item>
+                            <Avatar alt={thread.conclusion.user.name} src={''} />
+                        </Grid>
+                        <Grid justifyContent="left" item xs zeroMinWidth>
+                            <h4 style={{ margin: 0, textAlign: 'left' }}>{thread.conclusion.user.name}</h4>
+                            <h5 style={{ margin: 0, textAlign: 'left' }}>{thread.conclusion.user.department.departmentName}</h5>
+                            <p style={{ textAlign: 'left' }}>{thread.conclusion.content}</p>
+                            <p style={{ textAlign: 'left', color: 'gray' }}>{thread.conclusion.dateOfPost}</p>
+                        </Grid>
+                    </Grid>
+                    <Divider variant="fullWidth" style={{ margin: '30px 0' }} />
+                </Paper>
+            ) : null}
+            {isAdmin ? (
+                <div>
+                    <TextField
+                        required
+                        id="outlined-required"
+                        label="New Conclusion"
+                        value={newConclusion}
+                        onChange={(e) => setNewConclusion(e.target.value)}
+                    />
+                    <Button
+                        variant="contained"
+                        color="primary"
+                        onClick={() => handleAddConclusion("APPROVED")}
+                    >
+                        APPROVED
+                    </Button>
+                    <Button
+                        variant="contained"
+                        color="primary"
+                        onClick={() => handleAddConclusion("NOT APPROVED")}
+                    >
+                        NOT APPROVED
+                    </Button>
+
+                </div>
+            ) : null}
         </div>
-      );
-    };
+    );
+};
