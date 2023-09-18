62f1f9c8d576f9538ab7f24da70394e894293326
JowitaDruciak <jowita.druciak@gmail.com>
2023-09-18 22:07:44 +0200

    verify token and refresh token

--- client/src/authHelpers/authHelpers.ts
+++ client/src/authHelpers/authHelpers.ts
@@ -1,93 +1,82 @@
 import axios from "axios";
 import jwtDecode from "jwt-decode";
-import { UserContext } from "../context/UserContext";
-import useContext  from "react";
-
-
 
 export const getJwtToken = async () => {
-
-    try {
-        const jwtToken = localStorage.getItem("token");
+    const jwtToken = localStorage.getItem("token");
 
 
-        if (jwtToken === null || jwtToken.length === 0) {
-            return null
-        }
+    if (verifyIfTokenIsValid(jwtToken)) {
+        return jwtToken;
+    }
+    const jwtRefreshToken = localStorage.getItem("refresh_token");
 
-        const decodedToken = jwtDecode(jwtToken) as any;
-
-        const tokenExp = decodedToken.exp as number;
-
-        const currentTimestamp = Math.round(Date.now() / 1000);
-
-        if (tokenExp > currentTimestamp) {
-            return jwtToken;
-        }
-
-        //pobieramy refresh
-
-        const jwtRefreshToken = localStorage.getItem("refresh_token");
-
-        if (jwtRefreshToken === null || jwtRefreshToken.length === 0) {
-            return null // czy tu jeszcze wylogować
-        }
-
-        const decodedRefreshToken = jwtDecode(jwtRefreshToken) as any;
-
-        const refreshTokenExp = decodedRefreshToken.exp as number;
-
-        //sprawdzamy wazność refresh
-
-        if (!(refreshTokenExp > currentTimestamp)) {
-            //wylogowujemy uytkownika, usuwamy z localstorage
-        }
-
-        //jeśli wazny to zapytanie do back -> POST /refreshToken -> dostajemy nowy token i refresh
-            //zapisać do localStorage 
-            // zwracamy nowy jwtToken
-
-        //zapisać do localStorage 
-
-        // const token = await RequestHelper.handleAnyRequest(() =>
-        //     AuthService.getJwtToken()
-        // );
-
-        // if (!token) {
-        //     return null;
-        // }
-
-        // localStorage.setItem(jwtLocalStorageKey, token);
-
-        // return token;
-    } catch (error) {
+    if (!verifyIfTokenIsValid(jwtRefreshToken)) {
+        handleLogout();
         return null;
     }
-};
 
+    try {
+        const response = await fetchNewTokens(jwtRefreshToken!);
+        if (response.status === 200) {
+            saveTokensToLocaleStorage(response.data);
+            return jwtToken;
+        }
+    } catch (error: any) {
+        console.log("Błąd podczas odświeżania tokena:", error.message);
+    }
+}
+
+
+async function fetchNewTokens(jwtRefreshToken: string) {
+    return await axios.post("http://localhost:8080/api/v1/auth/refresh-token", null, {
+        headers: {
+            Authorization: `Bearer ${jwtRefreshToken}`,
+        },
+    });
+}
+
+function isTokenExpired(tokenExp: number, currentTimestamp: number) {
+    return tokenExp < currentTimestamp;
+}
+
+
+export function saveTokensToLocaleStorage({ access_token, refresh_token }: { access_token: string, refresh_token: string }) {
+    localStorage.setItem("token", access_token);
+    localStorage.setItem("refresh_token", refresh_token);
+}
+
+function verifyIfTokenIsValid(jwtToken: string | null) {
+    const currentTimestamp = Math.round(Date.now() / 1000);
+    if (jwtToken === null || jwtToken.length === 0) {
+        return false;
+    }
+    const decodedToken = jwtDecode(jwtToken) as any;
+    const tokenExp = decodedToken.exp as number;
+
+    return !isTokenExpired(tokenExp, currentTimestamp);
+}
 
 export const handleLogout = async () => {
     try {
-      const response = await axios.post(
-        'http://localhost:8080/api/v1/auth/logout',
-        {},
-        {
-          headers: {
-            Authorization: `Bearer ${localStorage.getItem('token')}`,
-          },
+        const response = await axios.post(
+            'http://localhost:8080/api/v1/auth/logout',
+            {},
+            {
+                headers: {
+                    Authorization: `Bearer ${localStorage.getItem('token')}`,
+                },
+            }
+        );
+
+        if (response.status === 200) {
+            console.log('User logged out on the server.');
+        } else {
+            console.log('Failed to log out user on the server.');
         }
-      );
-  
-      if (response.status === 200) {
-        console.log('User logged out on the server.');
-      } else {
-        console.log('Failed to log out user on the server.');
-      }
     } catch (error) {
-      console.log('An error occurred while logging out on the server:', error);
+        console.log('An error occurred while logging out on the server:', error);
     }
-  
+
     localStorage.clear();
-    sessionStorage.removeItem('token');
     // userContext.setUser(null);
-  };
+};
--- client/src/pages/Login/Login.tsx
+++ client/src/pages/Login/Login.tsx
@@ -5,6 +5,7 @@ import logo from "../../assets/logo.svg";
 import "./Login.css";
 import { styled } from "@mui/system";
 import { Button, TextField } from "@mui/material";
+import {saveTokensToLocaleStorage} from "../../authHelpers/authHelpers"
 
 interface LoginData {
   email: string;
@@ -131,10 +132,8 @@ const Login: React.FC = () => {
       if (response.ok) {
         console.log("Login successful!");
         const data: { access_token: string, refresh_token: string } = await response.json();
-        console.log(data);
         //localStorage.setItem("token", JSON.stringify(data.access_token)); // tutaj trzeba bylo usunac stringify bo robilo podwojny cudzyslow !
-        localStorage.setItem("token", data.access_token);
-        localStorage.setItem("refresh_token", data.refresh_token);
+        saveTokensToLocaleStorage(data);
         //localStorage.setItem("refresh_token", data.re);
         console.log(data)
         setUser(data.access_token); //lub nazwa uzytkownika?
