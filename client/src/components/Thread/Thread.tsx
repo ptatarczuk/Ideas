f0427d629a45b307bd30d5d1f1edc64572329593
JowitaDruciak <jowita.druciak@gmail.com>
2023-09-19 14:41:47 +0200

    edit thread with file

--- client/src/components/Thread/Thread.tsx
+++ client/src/components/Thread/Thread.tsx
@@ -2,7 +2,6 @@ import { TextField, Button, Box } from '@mui/material';
 import React, { useState } from 'react';
 import { Thread } from '../../models/Thread';
 import { getJwtToken } from '../../authHelpers/authHelpers'
-import { Description } from '@mui/icons-material';
 import axios from 'axios';
 
 
@@ -16,6 +15,7 @@ export const ThreadComponent: React.FC<ThreadComponentProps> = ({ thread, decode
     const [isEditing, setIsEditing] = useState<boolean>(false);
     const [buttonText, setButtonText] = useState<string>("EDIT");
     const isAuthor = editedThread.user.email === decodedToken.sub;
+    const [newImage, setNewImage] = useState<File | null>(null);
     const isUser = decodedToken.role === "User";
     const isAdmin = decodedToken.role === "Admin";
 
@@ -30,6 +30,18 @@ export const ThreadComponent: React.FC<ThreadComponentProps> = ({ thread, decode
     const handleEdit = () => {
         setIsEditing(true);
         setButtonText("SAVE");
+        console.log(thread)
+    };
+
+    const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const selectedFile = e.target.files?.[0];
+        if (selectedFile) {
+            setNewImage(selectedFile);
+        }
+    };
+
+    const handleRemoveImage = () => {
+        setNewImage(null);
     };
 
 
@@ -38,42 +50,44 @@ export const ThreadComponent: React.FC<ThreadComponentProps> = ({ thread, decode
         e.preventDefault();
         try {
             const jwtToken = await getJwtToken();
-            
             if (!jwtToken) {
                 return;
             }
-    
+
             const updatedThread = {
                 title: editedThread.title,
                 description: editedThread.description,
                 justification: editedThread.justification,
             };
-    
+
             const formData = new FormData();
             formData.append('thread', JSON.stringify(updatedThread));
-           
-    
+
+            if (newImage) {
+                formData.append('file', newImage);
+            }
+
             const config = {
                 headers: {
                     'Content-Type': 'multipart/form-data',
                     Authorization: `Bearer ${jwtToken}`,
                 },
             };
-    
+
             const response = await axios.patch(`http://localhost:8080/threads/id/${thread.threadId}`, formData, config);
-    
+
             if (response.status !== 200) {
                 const errorData = response.data;
                 throw new Error(`Network response was not ok. Error data: ${JSON.stringify(errorData)}`);
             }
-    
+
             setIsEditing(false);
             setButtonText("EDIT");
         } catch (error) {
             console.error('An error occurred while saving data', error);
         }
     };
-    
+
 
     return (
         <Box
@@ -105,7 +119,6 @@ export const ThreadComponent: React.FC<ThreadComponentProps> = ({ thread, decode
                 disabled={true}
                 value={editedThread.user.name}
             />
-            <img src={editedThread.photo} alt="Thread Photo" />
             <TextField
                 required
                 id="outlined-required"
@@ -122,6 +135,21 @@ export const ThreadComponent: React.FC<ThreadComponentProps> = ({ thread, decode
                 onChange={(e) => handleFieldChange('justification', e.target.value)}
                 disabled={!isEditing}
             />
+            {thread.photo && <img src={thread.photo} alt="Thread Photo" />}
+            {isEditing && (
+                <div>
+                    <input type="file" onChange={handleImageChange} style={{ maxWidth: '1000px' }} />
+                    <button onClick={handleRemoveImage}>Remove Image</button>
+                    {newImage && (
+                        <div>
+                            <img src={URL.createObjectURL(newImage)} alt="New Image" style={{ maxWidth: '100px' }} />
+                        </div>
+                    )}
+                </div>
+            )}
+            <div>
+            </div>
+
         </Box>
     );
 };
